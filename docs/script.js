7d7b60c89441441292419ecb07915b5f93022f1d
camiloDanielli <79849665+camiloDanielli@users.noreply.github.com>
2021-08-12 09:31:29 -0300

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ const calculo = () => {
  const rd = document.getElementById("rendimiento").value;
  const gas = document.getElementById("tipo").value;
  const x = km/rd;
- const y = x*gas;
+ const y = Math.round(x*gas);
 console.log(y);
 
  document.getElementById("result").innerHTML=y+"$"+" pesos";
